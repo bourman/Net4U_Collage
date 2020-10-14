3f83e1ffe560547634d273f051d85f03df0f015e
Victor Bourman <68653187+bourman@users.noreply.github.com>
2020-10-14 03:15:35 +0300

    install fast python in ubuntu18.04 for me.
    fast script for install python in ubuntu
    #bash install_python_ubutnu18_04.sh

---
+++ install_python_ubutnu18_04.sh
@@ -0,0 +1,10 @@
+#fast install for me (victor bourman)
+echo -e "Welcome to install automatic of python3.8"
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.8
+sudo apt install python3
+sudo apt install python
+echo "Done"
